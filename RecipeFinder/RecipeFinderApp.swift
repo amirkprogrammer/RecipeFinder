496cb46ac897b2d726f37dc7f581c72d3ad5ba6a
amirkprogrammer <amir.m.kabiri@gmail.com>
2024-06-30 21:09:17 -0700

    Initial Commit

---
+++ RecipeFinder/RecipeFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeFinderApp.swift
+//  RecipeFinder
+//
+//  Created by Amir Kabiri on 6/30/24.
+//
+
+import SwiftUI
+
+@main
+struct RecipeFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
